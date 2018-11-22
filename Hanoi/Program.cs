ca0b2c0bfac388c7e3b0603dc0a37c917ad76da0
Andree-Anne Roy <andree-anne.roy2009@hotmail.com>
2018-11-21 19:22:49 -0500

    Add project files.

---
+++ Hanoi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char debut = '1'; //Indique la position de début (à la sortie).
+            char fin = '2'; //Indique la position de fin (à la sortie).
+            char temporaire = '3'; //Indique la position temporaire (à la sortie).
+            int nbTotalDisques = 3; //Nombre total de disques
+
+            resoudreJeuHanoi(nbTotalDisques, debut, fin, temporaire);
+
+            Console.ReadLine();
+        }
+
+        private static void resoudreJeuHanoi(int nbDisques, char debut, char fin, char temp)
+        {
+            if (nbDisques > 0)
+            {
+                resoudreJeuHanoi(nbDisques - 1, debut, temp, fin);
+                Console.WriteLine("On bouge le disque " + debut + " vers " + fin);
+                resoudreJeuHanoi(nbDisques - 1, temp, fin, debut);
+            }
+        }
+    }
+}
